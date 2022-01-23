c5cf99428112ae1e33aa1bc71299527d7a0fa562
nis1283 <94506906+nis1283@users.noreply.github.com>
2022-01-23 14:29:04 +0900

    move to git

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "5132c6a00298e512e62d4c84fdbeb2d5";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = ` | ${data.main.temp}°C`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
